1395534e27bf0144c47051c68fe7516147219db9
kmlcburce <kmlcburce@gmail.com>
2023-10-17 20:34:40 +0800

    update

---
+++ app/Http/Controllers/StaffController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaffController extends Controller
+{
+    //
+    function create(Request $request){
+        $data = $request->all();
+    }
+}
---
+++ database/migrations/2023_10_17_121322_create_staff_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id('user_id')->unique();
+            $table->string('password');
+            $table->integer('account_type');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
