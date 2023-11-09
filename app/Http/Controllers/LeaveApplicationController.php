24531bb2aa79f70d30376f9da80342bc96306d0b
kmlcburce <kmlcburce@gmail.com>
2023-11-09 14:45:01 +0800

    leaves

--- app/Http/Controllers/LeaveApplicationController.php
+++ app/Http/Controllers/LeaveApplicationController.php
@@ -13,14 +13,23 @@ class LeaveApplicationController extends APIController
 {
     public function create(Request $request){
         $data = $request->all();
-        $LeaveUuid = Str::orderedUuid();
+        try{
+            $LeaveUuid = Str::orderedUuid();
 
-        $leave = new LeaveApplication();
-        $leave->id = $LeaveUuid;
-        $leave->user_id = $data['user_id'];
-        $leave->leave_start = $data['leave_start'];
-        $leave->leave_end = $data['leave_end'];
-        $table->leave_reason = $data['leave_reason'];
-        $table->status = 'pending'; 
+            $leave = new LeaveApplication();
+            $leave->id = $LeaveUuid;
+            $leave->user_id = $data['user_id'];
+            $leave->leave_start = $data['leave_start'];
+            $leave->leave_end = $data['leave_end'];
+            $leave->leave_reason = $data['leave_reason'];
+            $leave->status = 'pending'; 
+            $leave->save();
+            $this->response['data'] = "Created";
+            return $this->getResponse();
+        }catch(\Throwable $th){
+            $this->response['error'] = $th->getMessage();
+            $this->response['status'] = $th->getCode();
+            return $this->getResponse();
+        }
     }
 }
